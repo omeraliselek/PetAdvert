779ba44817fef270cdf1f884e8cd575d6c6cb8b4
Taner SELEK <bentanerselek@gmail.com>
2019-05-04 16:35:42 +0300

    DTO

---
+++ PetAdvert.UI/Areas/Admin/Models/DTO/AdvertisementDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetAdvert.UI.Areas.Admin.Models.DTO
+{
+    public class AdvertisementDTO:BaseDTO
+    {
+    }
+}
